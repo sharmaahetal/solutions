d4d876c50e907e7fe8a3a008afd0e696adf59f0c
sharmaahetal <sharmaahetal@gmail.com>
2026-06-04 13:03:45 +0530

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.cpp
@@ -0,0 +1,70 @@
+class Node{
+public:   
+    int key;
+    int value;
+    Node* next;
+    Node* prev;
+
+    Node(int k , int v){
+        key = k;
+        value = v;
+        prev = next = nullptr;
+    }
+};
+class LRUCache {
+private:
+    int cap;
+    unordered_map<int , Node*> mp;
+    Node* left;
+    Node* right;
+
+    void remove(Node* node){
+        Node* prev = node->prev;
+        Node* nxt = node->next;
+        nxt->prev = prev;
+        prev->next = nxt;
+    }
+    void insert(Node* node){
+        Node* prev = right->prev;
+        prev->next = node;
+        node->next = right;
+        node->prev = prev;
+        right->prev = node;
+    }
+
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+        mp.clear();
+        left = new Node(0,0);
+        right = new Node(0,0);
+        left->next = right;
+        right->prev = left;
+    }
+    
+    int get(int key) {
+        if(mp.find(key) != mp.end()){
+            Node* node = mp[key];
+            remove(node);
+            insert(node);
+            return node->value;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key) != mp.end()){
+            remove(mp[key]);
+        }
+        Node* newNode = new Node(key , value);
+        mp[key] = newNode;
+        insert(newNode);
+
+        if(mp.size() > cap){
+            Node* lru = left->next;
+            remove(lru);
+            mp.erase(lru->key);
+            delete lru;
+        }
+    }
+};
