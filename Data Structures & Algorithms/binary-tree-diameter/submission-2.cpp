15c87a4912b1ef35fc8ebaa1207f8309938728d3
sharmaahetal <sharmaahetal@gmail.com>
2026-06-07 20:03:17 +0530

    Add: binary-tree-diameter - submission-2

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-2.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        unordered_map<TreeNode* , pair<int,int>> mp;
+        mp[nullptr] = {0,0};
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()){
+            TreeNode* node = st.top();
+
+            if(node->left && mp.find(node->left) == mp.end()){
+                st.push(node->left);
+            }
+            else if(node->right && mp.find(node->right) == mp.end()){
+                st.push(node->right);
+            }
+            else{
+                node = st.top();
+                st.pop();
+
+                auto[leftHeight , leftDiameter] = mp[node->left];
+                auto[rightHeight , rightDiameter] = mp[node->right];
+
+                int height = 1 + std::max(leftHeight , rightHeight);
+                int diameter = max(leftHeight + rightHeight , max(rightDiameter , leftDiameter));
+
+                mp[node] = {height , diameter};
+            }
+        }
+        return mp[root].second;
+    }
+};
