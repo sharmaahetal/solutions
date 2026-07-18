f0bac692b2aa85c06f4492ca56ea42f4dacf3d45
sharmaahetal <sharmaahetal@gmail.com>
2026-07-18 19:06:55 +0530

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool issafe(vector<string> &board, int row, int col, int n){
+        //horizontal
+        for(int j=0; j<n; j++){
+            if(board[row][j] == 'Q'){
+                return false;
+            }
+        }
+        //vertical
+        for(int i=0; i<n ; i++){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        //left diagonal
+        for(int i=row , j=col ; i>=0 && j>=0 ; i--, j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        //right diagonal
+        for(int i=row, j=col ; i>=0 && j>=0 ; i--, j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    //place queens recusrsively in n different rows;
+    void nqueens(vector<string> &board, int row, int n, vector<vector<string>> &ans){
+        if(row == n){
+            ans.push_back({board});
+            return;
+        }
+        //check all possible cols for we can place queen
+        for(int j=0; j<n ; j++){
+            if(issafe(board, row, j, n)){
+                board[row][j] = 'Q';
+                nqueens(board, row+1, n, ans);
+                board[row][j] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n , '.'));
+        vector<vector<string>> ans;
+
+        nqueens(board, 0, n, ans);
+        return ans;
+    }
+};
