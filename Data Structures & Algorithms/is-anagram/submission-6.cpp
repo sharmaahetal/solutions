74d00a97781ff0e3f2a8ec94b4ca20bee21253d6
sharmaahetal <sharmaahetal@gmail.com>
2026-09-08 18:48:33 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        unordered_map<char,int> freq;
+
+        for(int i=0; i<s.size(); i++){
+            freq[s[i]]++;
+            freq[t[i]]--;
+        }
+        for(auto x : freq){
+            if(x.second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
